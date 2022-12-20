368d154938977037fb43380e6d79cd9be0a30966
urba <patryk.urba@nokia.com>
2022-12-20 11:01:22 +0100

    initial push

---
+++ include/Monopoly.hpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <utility>
+
+
+class Monopoly
+{
+public:
+
+    Monopoly(std::vector<IPlayer> p_players, Board p_board)
+        : m_players(std::move(p_players)), m_board(std::move(p_board)) {}
+
+
+private:
+
+    std::vector<IPlayer> m_players;
+    Board m_board;
+
+}
